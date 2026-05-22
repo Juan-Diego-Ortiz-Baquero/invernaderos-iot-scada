d1f41509d48bc4590026d10eecfd8c0e360b951f
JuanOrtiz <jdiegoortiz@ucundinamarca.edu.co>
2026-05-22 18:01:43 -0500

    cambio de red1

--- projectbd/projectbd.ino
+++ projectbd/projectbd.ino
@@ -10,8 +10,8 @@
 #include <DHT.h>
 
 // ── WiFi ──────────────────────────────────────────────────────
-const char* WIFI_SSID     = "Troyanos gratis";
-const char* WIFI_PASSWORD = "zwci5043";
+const char* WIFI_SSID     = "Redmi Note 13 5G"; 
+const char* WIFI_PASSWORD = "123456789*";
 
 // ── API ───────────────────────────────────────────────────────
 const char* API_URL = "https://api-invernaderos-inicial.onrender.com/api/Lecturas";
